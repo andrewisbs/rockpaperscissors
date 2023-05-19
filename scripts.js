a1c8a1b10aee1c4d74a48643a80770f1a87920e1
Andrewisbs <andrew@bestsober.com>
2023-05-19 10:05:56 -0500

    add switch to turn input into number

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 //getComputerChoice 
 
-const ROCK = 1;
-const PAPER = 2;
-const SCISSORS = 3;
+//const ROCK = 1;
+//const PAPER = 2;
+// SCISSORS = 3;
 
 function getComputerChoice() {
     //This function will Randomly return a value of “rock” “paper” or “scissors”
@@ -18,12 +18,33 @@ function randomNumber() {
     return end;
 }
 
-console.log(getComputerChoice());
-
 function playRound(computerChoice) {
 
-    let humanChoice = prompt("Give me rock, paper, scissors");
-    console.log(humanChoice);
+    let humanInput = prompt("Give me rock, paper, scissors");
+    let humanInputUpper = humanInput.toUpperCase(humanInput);
+    let humanNumber;
+    switch(humanInputUpper) {
+        case "ROCK":
+            humanNumber = 1;
+            break;
+
+        case "PAPER":
+            humanNumber = 2;
+            break;
+
+        case "SCISSORS":
+            humanNumber = 3;
+            break;
+        
+        default:
+            humanNumber = 4;
+            console.error("meaninglessness");
+            break;
+        
+    }
+    //compare that number to computer, and declare winner
+    //toUpperCase()
+    console.log(humanNumber);
 
 }
 
